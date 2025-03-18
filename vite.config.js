8e17b81343208d4586cdb4dd1909b2b142e274ef
DolceTseng <jessica.turn613@gmail.com>
2025-03-18 19:30:38 +0800

    add folder notes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from 'vite-plugin-react-mdx'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mdx()],
   base: process.env.NODE_ENV === 'production' ? ' /obsidian-public-notes/ ' : '/'
 })
